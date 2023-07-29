adf9ff26f947448efbf40e14f68e163a5942e106
Jonathan Guerra <jonalves91@gmail.com>
2023-07-28 23:55:27 -0300

    Delete src directory

--- src/main/java/com/guerradev/schoolboard/repositories/AlunoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.guerradev.schoolboard.repositories;
-
-import com.guerradev.schoolboard.model.Aluno;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlunoRepository extends JpaRepository<Aluno, Long> {
-}
